53296d4c65538f6e33d4ffe4ac91e97e62b426d7
joco1204 <joco1204@gmail.com>
2018-08-29 17:21:38 -0500

    JC
    JC

--- app/model/reportes/reporte_general.php
+++ app/model/reportes/reporte_general.php
@@ -25,8 +25,7 @@ $query .= "WHERE a.id_empresa = '".$_GET['empresa']."' AND a.id_campana = '".$_G
 $query .= "ORDER BY a.id; ";
 $result = $db->query($query);
 $result_h = $db->query($query);
-$i = 0;
-$j = 0;
+$i = 1;
 ?>
 <table border="1">
 	<thead>
@@ -42,18 +41,23 @@ $j = 0;
 			<th>TIPIFICACIONES</th>
 			<th>SOLUCI&Oacute;N</th>
 			<th>AUDIO</th>
+
 			<?php while($row_h = $result_h->fetch(PDO::FETCH_OBJ)){
-				$i++;
+				$id_h = $row_h->id_monitoreo;
 				$query_th  = "SELECT id FROM ca_monitoreo_asesor_detallado WHERE id_monitoreo_asesor = '".$row_h->id_monitoreo."'; ";
 				$result_th = $db->query($query_th);
-				while($row_th = $result_th->fetch(PDO::FETCH_OBJ)){
-					$j++; 
+				$j = 1;
+				while($row_th = $result_th->fetch(PDO::FETCH_OBJ)){ 
+					$id_th = $row_th->id;
 				?>
 					<th>Item <?php echo $i.".".$j; ?></th>
 					<th>Porcentaje <?php echo $i.".".$j; ?></th>
 					<th>Punto Entrenamiento <?php echo $i.".".$j; ?></th>
-				<?php } ?>
-			<?php } ?>
+				<?php $j++; } ?>
+			<?php 
+				$i++; 
+				} 
+			?>
 		</tr>
 	</thead>
 	<tbody>
